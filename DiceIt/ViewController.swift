f393e146469c97c001f3ee81cda24bf938a83de2
Brakaat <43380552+Brakaat@users.noreply.github.com>
2020-05-13 22:35:33 +0800

    v2

--- DiceIt/ViewController.swift
+++ DiceIt/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        
         //使用Int.random函式來顯示隨機range
 //        diceImageView1.image = diceArray[Int.random(in: 0...5)]
 //        diceImageView2.image = diceArray[Int.random(in: 0...5)]
