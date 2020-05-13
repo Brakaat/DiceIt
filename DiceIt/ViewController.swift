a43000caf8f743064f3d9c09ef7cea01623e2b6a
Brakaat <43380552+Brakaat@users.noreply.github.com>
2020-05-13 22:34:11 +0800

    Update ViewController.swift
    v2

--- DiceIt/ViewController.swift
+++ DiceIt/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Interface Builder Outlet
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        //使用Int.random函式來顯示隨機range
+//        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+//        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+        
+        //使用陣列.randomElement()來隨機取樣
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+    }        
 }
-
